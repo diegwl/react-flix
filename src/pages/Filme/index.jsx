c8ba14327115c94d37217c6bf45b562c012c6784
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-08-30 11:30:28 -0300

    tela do filme e trailer

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { StyledFilmeInfo, StyledImg, StyledSinopse, StyledSinopseTitle, StyledTitle, StyledButton, StyledButtonLink } from './styles'
 
 import { api, API_KEY } from '../../services/api'
 
 function Filme(){
     const { id } = useParams();
     const [filme, setFilme] = useState({});
+    const [trailer, setTrailer] = useState('');
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function loadFilme() {
-            await api.get(`/movie/{id}`, {
+            await api.get(`/movie/${id}`, {
                 params: {
                     api_key: API_KEY,
                     language: 'pt-BR',
@@ -21,33 +25,57 @@ function Filme(){
                 setLoading(false);
             })
             .catch(() => {
+                navigate("/", { replace: true })
+                return;
+            })
+        }
 
+        async function loadTrailer() {
+            await api.get(`/movie/${id}/videos`, {
+                params: {
+                    api_key: API_KEY,
+                    language: 'pt-BR',
+                },
+            })
+            .then((response) => {
+                setTrailer(response.data.results[0].key)
             })
         }
 
         loadFilme();
+        loadTrailer();
 
         return () => {
-                
+            
         }
-    }, []);
+    }, [id, navigate]);
 
     if(loading){
         return(
-            <div className='filme-info'>
+            <StyledFilmeInfo>
                 <h1>Carregando detalhes...</h1>
-            </div>
+            </StyledFilmeInfo>
         )
     }
 
     return(
-        <div className='filme-info'>
-            <h1>{filme.title}</h1>
-            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+        <StyledFilmeInfo>
+            <StyledTitle>{filme.title}</StyledTitle>
+            <StyledImg src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
             
-            <h3>Sinopse</h3>
-            <span>{filme.overview}</span>
-        </div>
+            <StyledSinopseTitle>Sinopse</StyledSinopseTitle>
+            <StyledSinopse>{filme.overview}</StyledSinopse>
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+            <div>
+                <StyledButton>Salvar</StyledButton>
+                <StyledButton>
+                    <StyledButtonLink target='blank' href={`https://www.youtube.com/watch?v=${trailer}`}>
+                        Trailer
+                    </StyledButtonLink>
+                </StyledButton>
+            </div>
+        </StyledFilmeInfo>
     )
 }
 
---
+++ src/pages/Filme/styles.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const StyledFilmeInfo = styled.div`
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    padding: 0 8px;
+    margin: 0 auto;
+`;
+
+export const StyledTitle = styled.h1`
+    margin: 14px 0;
+`;
+
+export const StyledImg = styled.img`
+    border-radius: 8px;
+    width: 800px;
+    max-width: 100%;
+    max-height: 340px;
+    object-fit: cover;
+`;
+
+export const StyledSinopseTitle = styled.h3`
+    margin-top: 14px;
+`;
+
+export const StyledSinopse = styled.span`
+    margin: 8px 0;
+`;
+
+export const StyledButton = styled.button`
+    margin-top: 14px;
+    margin-left: 0;
+    margin-right: 12px;
+    font-size: 20px;
+    border: 0;
+    outline: none;
+    padding: 12px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-bottom: 60px;
+    transition: all 0.3s;
+    &:hover {
+        color: #747bff;
+    }
+`;
+
+export const StyledButtonLink = styled.a`
+    text-decoration: none;
+`;
